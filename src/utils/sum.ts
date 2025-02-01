f3e630f2fccfdf60635e92481b899d1703bae1c4
zigota <sippin4romthehell@gmail.com>
2025-02-01 22:11:48 +0300

    chore: add jsdoc

--- src/utils/sum.ts
+++ src/utils/sum.ts
@@ -1,3 +1,9 @@
 import { SumFn } from "../types";
 
+/**
+ * Функция которая складывает числа
+ * @param {number} a первое число
+ * @param {number} b второе число
+ * @returns {number} итоговое значение суммы
+ */
 export const sum : SumFn = (a, b) => a + b; 
